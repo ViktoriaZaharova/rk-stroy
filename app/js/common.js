d4a239bb0474910a9f0c6621a0088c705203e9ef
viktoriazaharova <zaharova181114@gmail.com>
2020-03-30 12:07:27 +0300

    media

--- app/js/common.js
+++ app/js/common.js
@@ -7,8 +7,41 @@ $('[data-fancybox="portfolio"]').fancybox();
 //     autoplaySpeed: 2000,
 // });
 
+// slick active
+$(window).on('load resize', function() {
+    if ($(window).width() < 992) {
+        $('.footer-box h3').click(function () {
+           $(this).toggleClass('click').siblings('.footer-menu').fadeToggle();
+        });
+    }
+    if ($(window).width() < 576) {
+        $('.employees__content:not(.slick-initialized)').slick({
+            dots: true,
+            infinite: true,
+            speed: 1000,
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            arrows: false,
+            enterMode: true,
+            variableWidth: true
+        });
+    } else {
+        $(".employees__content.slick-initialized").slick("unslick");
+    }
+});
+// slick active
+
 $("body").on("click", ".btn-up", function () {
     $("html, body").animate({
         scrollTop: 0
     }, "slow")
+});
+
+$('.btn-burger').click(function () {
+   $('.header-bottom').fadeToggle();
+
+});
+
+$('.btn-close').click(function () {
+   $('.header-bottom').fadeOut();
 });
