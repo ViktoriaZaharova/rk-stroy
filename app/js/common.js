20e8c6250c87c78526b95971c6c7f1fa02e0edb5
viktoriazaharova <zaharova181114@gmail.com>
2020-04-01 14:44:25 +0300

    modal + edit css

--- app/js/common.js
+++ app/js/common.js
@@ -32,12 +32,12 @@ $("body").on("click", ".btn-up", function () {
 });
 
 $('.btn-burger').click(function () {
-   $('.header-bottom').fadeToggle();
+   $('.header-bottom__mobile').fadeToggle();
 
 });
 
 $('.btn-close').click(function () {
-   $('.header-bottom').fadeOut();
+   $('.header-bottom__mobile').fadeOut();
 });
 
 // header scroll
@@ -51,14 +51,58 @@ $(window).scroll(function () {
 
 //плавный скролл
 $(document).ready(function () {
-    $('.go_to').click(function () {
-        var scroll_el = $(this).attr('href');
-        if ($(scroll_el).length != 0) {
-            $('html, body').animate({
-                scrollTop: $(scroll_el).offset().top
-            }, 500);
-        }
-        return false;
+    $(".header-bottom__desktop .go_to").on("click", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body, html').animate({scrollTop: top - 160}, 500);
+    });
+});
+
+
+$(document).ready(function () {
+    $(".header-bottom__mobile .go_to").on("click", function (event) {
+        event.preventDefault();
+        var id = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body, html').animate({scrollTop: top - 90}, 500);
+        $('.header-bottom__mobile').fadeOut();
     });
 });
 //плавный скролл end
+
+// модальные окна (несколько)
+$(document).ready(function () {
+    var overlay = $('.overlay');
+    var open_modal = $('.open_modal');
+    var close = $('.modal__close, .overlay');
+    var modal = $('.modal__div');
+
+    open_modal.click(function (event) {
+        event.preventDefault();
+        var div = $(this).attr('href');
+        overlay.fadeIn(400,
+            function () {
+                $(div)
+                    .css('display', 'flex')
+                    .animate({
+                        opacity: 1,
+                        top: '50%'
+                    }, 200);
+            });
+    });
+
+    close.click(function () {
+        modal
+            .animate({
+                    opacity: 0,
+                    top: '45%'
+                }, 200,
+                function () {
+                    $(this).css('display', 'none');
+                    overlay.fadeOut(400);
+                }
+            );
+    });
+});
+//end
